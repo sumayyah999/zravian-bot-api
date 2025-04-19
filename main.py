36b421f9f71cfbd40676af06fb19a36a9371b6ae
sumayyah999 <novindraadiwira@gmail.com>
2025-04-19 06:44:28 +0000

    Fix port to use os.environ.get

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def home():
     return "Hello from Railway!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
